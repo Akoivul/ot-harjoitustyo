cb31d79beb903fc04082a9b535209c8b244678d4
aarokoivula <aaro.koivula@gmail.com>
2026-03-31 21:00:51 +0300

    add build invoke task

--- tasks.py
+++ tasks.py
@@ -12,3 +12,7 @@ def test(ctx):
 def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
+
+@task
+def build(ctx):
+    ctx.run("python3 src/initialize_database.py", pty=True)
